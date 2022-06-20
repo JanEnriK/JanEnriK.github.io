914d2e60def94b1a7f66360478348f13f02d578e
JanEnriK <99331552+JanEnriK@users.noreply.github.com>
2022-06-20 15:25:43 +0800

    Add files via upload

---
+++ Jan Enrique Manuel/PHPHelloWorldphp.php
@@ -0,0 +1,17 @@
+<!DOCRYPE HTML>
+<html lang= "en">
+
+<head>
+	<style type="text/css">
+	</style>
+	<title></title>
+</head>
+<body>
+	<link rel="stylesheet" type="text/css" href=" " />
+	<?php
+	echo "Hello World <br>";
+	echo "Welcome to php";
+	?>
+
+</body>
+</html>
---
+++ Jan Enrique Manuel/PHPTemplate.php
@@ -0,0 +1,17 @@
+<!DOCRYPE HTML>
+<html lang= "en">
+
+<head>
+	<style type="text/css">
+	</style>
+	<title></title>
+</head>
+<body>
+	<link rel="stylesheet" type="text/css" href=" " />
+	<?php
+	echo "Hello World";
+	echo "Welcome";
+	?>
+
+</body>
+</html>
